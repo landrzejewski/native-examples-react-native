22e923d30f7d865525d4d27d001c78ad0dbd78c7
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-03-06 11:51:47 +0100

    init

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const App = () => {
+  return (
+    <Text>Native</Text>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
